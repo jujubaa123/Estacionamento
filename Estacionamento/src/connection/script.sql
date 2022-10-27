56257229b458660b172c9652570f411fc6651ee7
03832903097 <03832903097@LAB205-16.fw.iffar.edu.br>
2022-10-27 10:09:23 -0300

    semana 5 - crud motorista

--- Estacionamento/src/connection/script.sql
+++ Estacionamento/src/connection/script.sql
@@ -17,3 +17,14 @@ CREATE TABLE  vaga(
     rua varchar(100) NOT NULL,
     obliqua boolean NOT NULL, 
     PRIMARY KEY (idVaga));
+
+CREATE TABLE  motorista(
+    idMotorista int NOT NULL AUTO_INCREMENT,
+    nome varchar(100) NOT NULL,
+    genero varchar(100) NOT NULL, 
+    rg int NOT NULL,
+    cpf int NOT NULL,
+    celular int NOT NULL,
+    email varchar(200) NOT NULL,
+    senha varchar(50) NOT NULL,
+    PRIMARY KEY (idMotorista));
